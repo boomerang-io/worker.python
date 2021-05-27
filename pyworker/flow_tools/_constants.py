3cb8a4cee238ca54c98c416a61748948d81457fc
Iulian Corcoja <iulian@corcoja.com>
2021-05-28 01:09:26 +0200

    Update project settings

--- pyworker/flow_tools/_constants.py
+++ pyworker/flow_tools/_constants.py
@@ -1,3 +1,4 @@
 TASK_INPUT_FILES = [
-    "/props/task.input.properties", "./props/task.input.properties", "tests/input_params_test1.txt"
+    "/props/task.input.properties", "./props/task.input.properties",
+    "tests/input_params_test1.txt"
 ]
